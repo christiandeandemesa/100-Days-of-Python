a293090e0f6e10741cca2d2d44c3e9398bba117d
Christian Dean <christiandeandemesa@gmail.com>
2023-08-20 20:07:23 -0700

    60 contact form

--- updated-jinja-blog/app.py
+++ updated-jinja-blog/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 posts = requests.get("https://api.npoint.io/b816686a3de82335626a").json()
+EMAIL="c53539588@gmail.com"
+PASSWORD="dicnhermaigxdhnr"
 
 app = Flask(__name__)
 
@@ -13,9 +16,22 @@ def get_all_posts():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+
+        return render_template("contact.html", msg_sent=True)
+    
+    return render_template("contact.html", msg_sent=False)
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(EMAIL, EMAIL, email_message)
 
 @app.route("/post/<int:index>")
 def show_post(index):
